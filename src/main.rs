c76596bbb67ea9e7be0992fd566149134b0cf948
Marius <K>
2021-03-23 23:39:58 +0100

    Add some new Stuff

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ use serde::{Deserialize, Serializer, Deserializer};
 use std::collections::HashMap;
 use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 
-#[derive(Deserialize, Debug)]
-struct Record {
-    CURRENCY: Value,
-    CURRENCY_DENOM: Value,
+#[derive(Deserialize, Debug, Clone)]
+struct Currency_Exchange_Entry {
+    CURRENCY: String,
+    CURRENCY_DENOM: String,
     #[serde(with = "my_date_format")]
-    TIME_PERIOD: DateTime<Utc>,
+    TIME_PERIOD: NaiveDate,
     OBS_VALUE: f64,
-    UNIT: String,
-    TITLE: String,
+}
+
+#[derive(Clone)]
+struct Currency_Exchange {
+    exchange_entry : Vec<Currency_Exchange_Entry>,
+    base_CURRENCY: String,
+    target_CURRENCY: String,
+    first_date: NaiveDate,
+    end_date: NaiveDate,
 }
 
 mod my_date_format {
@@ -20,11 +27,10 @@ mod my_date_format {
     use serde::{self, Deserialize, Deserializer};
     use chrono::format::Fixed;
 
-    pub fn deserialize<'de, D>(deserializer: D,) -> Result<DateTime<Utc>, D::Error> where D: Deserializer<'de>,
+    pub fn deserialize<'de, D>(deserializer: D,) -> Result<NaiveDate, D::Error> where D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        println!("s= {:?}", s);
-        let utc = Utc.datetime_from_str(&s, "%Y-%m-%d").map_err(serde::de::Error::custom);
+        let utc = NaiveDate::parse_from_str(&s, "%Y-%m-%d").map_err(serde::de::Error::custom);
         return utc;
     }
 }
@@ -36,12 +42,24 @@ fn hello_world() -> Result<i32, Box<dyn std::error::Error>> {
     let respons = package.send()?.text().unwrap();
 
     let mut rdr = csv::Reader::from_reader(respons.as_bytes());
-    for result in rdr.deserialize() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record:Record = result?;
-        println!("{:?}", record);
+    let mut deserial_result = rdr.deserialize();
+
+    let mut vec_of_entrys: Vec<Currency_Exchange_Entry> = Vec::new();
+    for (i,result) in deserial_result.enumerate() {
+        let record:Currency_Exchange_Entry = result?;
+        vec_of_entrys.push(record);
     }
+
+    let base_CURRENCY = vec_of_entrys[0].CURRENCY.clone();
+    let target_CURRENCY= vec_of_entrys[0].CURRENCY_DENOM.clone();
+    let first_date= vec_of_entrys[0].TIME_PERIOD.clone();
+    let end_date= vec_of_entrys[vec_of_entrys.len()-1].TIME_PERIOD.clone();
+
+
+    let USD_Exchange: Currency_Exchange = Currency_Exchange{exchange_entry: vec_of_entrys, base_CURRENCY, target_CURRENCY,
+        first_date, end_date
+    };
+
     Ok(-1)
 }
 

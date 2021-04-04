005c6e2e89f1f1886e910dd714ba851d47fea0ae
Marius <K>
2021-04-05 00:10:47 +0200

    first running version (h)

--- src/Currency_Exchange.rs
+++ src/Currency_Exchange.rs
@@ -1,20 +1,47 @@
 use std::collections::HashMap;
 use std::ops::Index;
-
 use reqwest::Response;
-
 use Currency::Exchange_History;
 use chrono::NaiveDate;
 use crate::Currency_Exchange::Currency::Currency_History_Entry;
 
 mod Currency;
 
-const THRESHOLD: i32 = 10;
-
 #[derive(Debug, Clone)]
 pub enum Currency_CODE{
     USD,
     EUR,
+    JPY,
+    BGN,
+    CZK,
+    DKK,
+    GBP,
+    HUF,
+    PLN,
+    RON,
+    SEK,
+    CHF,
+    ISK,
+    NOK,
+    HRK,
+    RUB,
+    TRY,
+    AUD,
+    BRL,
+    CAD,
+    CNY,
+    HKD,
+    IDR,
+    ILS,
+    INR,
+    KRW,
+    MXN,
+    MYR,
+    NZD,
+    PHP,
+    SGD,
+    THB,
+    ZAR,
     NONE,
 }
 
@@ -23,6 +50,36 @@ impl Currency_CODE{
         match self{
             Currency_CODE::EUR => return "EUR".to_string(),
             Currency_CODE::USD => return "USD".to_string(),
+            Currency_CODE::AUD => return "JPY".to_string(),
+            Currency_CODE::EUR => return "BGN".to_string(),
+            Currency_CODE::USD => return "CZK".to_string(),
+            Currency_CODE::AUD => return "DKK".to_string(),
+            Currency_CODE::EUR => return "GBP".to_string(),
+            Currency_CODE::USD => return "HUF".to_string(),
+            Currency_CODE::AUD => return "PLN".to_string(),
+            Currency_CODE::EUR => return "RON".to_string(),
+            Currency_CODE::USD => return "SEK".to_string(),
+            Currency_CODE::AUD => return "CHF".to_string(),
+            Currency_CODE::EUR => return "ISK".to_string(),
+            Currency_CODE::USD => return "NOK".to_string(),
+            Currency_CODE::AUD => return "HRK".to_string(),
+            Currency_CODE::EUR => return "RUB".to_string(),
+            Currency_CODE::USD => return "TRY".to_string(),
+            Currency_CODE::AUD => return "AUD".to_string(),
+            Currency_CODE::EUR => return "BRL".to_string(),
+            Currency_CODE::USD => return "CAD".to_string(),
+            Currency_CODE::AUD => return "CNY".to_string(),
+            Currency_CODE::EUR => return "HKD".to_string(),
+            Currency_CODE::USD => return "IDR".to_string(),
+            Currency_CODE::AUD => return "ILS".to_string(),
+            Currency_CODE::EUR => return "KRW".to_string(),
+            Currency_CODE::USD => return "MXN".to_string(),
+            Currency_CODE::AUD => return "MYR".to_string(),
+            Currency_CODE::EUR => return "NZD".to_string(),
+            Currency_CODE::USD => return "PHP".to_string(),
+            Currency_CODE::AUD => return "SGD".to_string(),
+            Currency_CODE::USD => return "THB".to_string(),
+            Currency_CODE::AUD => return "ZAR".to_string(),
             _ => "NONE".to_string()
         }
     }
@@ -31,6 +88,36 @@ impl Currency_CODE{
         match code{
             "EUR" => return Currency_CODE::EUR,
             "USD" => return Currency_CODE::USD,
+            "JPY" => return Currency_CODE::JPY,
+            "BGN" => return Currency_CODE::BGN,
+            "CZK" => return Currency_CODE::CZK,
+            "DKK" => return Currency_CODE::DKK,
+            "GBP" => return Currency_CODE::GBP,
+            "HUF" => return Currency_CODE::HUF,
+            "PLN" => return Currency_CODE::PLN,
+            "RON" => return Currency_CODE::RON,
+            "SEK" => return Currency_CODE::SEK,
+            "CHF" => return Currency_CODE::CHF,
+            "ISK" => return Currency_CODE::ISK,
+            "NOK" => return Currency_CODE::NOK,
+            "HRK" => return Currency_CODE::HRK,
+            "RUB" => return Currency_CODE::RUB,
+            "TRY" => return Currency_CODE::TRY,
+            "AUD" => return Currency_CODE::AUD,
+            "BRL" => return Currency_CODE::BRL,
+            "CAD" => return Currency_CODE::CAD,
+            "CNY" => return Currency_CODE::CNY,
+            "HKD" => return Currency_CODE::HKD,
+            "IDR" => return Currency_CODE::IDR,
+            "ILS" => return Currency_CODE::ILS,
+            "KRW" => return Currency_CODE::KRW,
+            "MXN" => return Currency_CODE::MXN,
+            "MYR" => return Currency_CODE::MYR,
+            "NZD" => return Currency_CODE::NZD,
+            "PHP" => return Currency_CODE::PHP,
+            "SGD" => return Currency_CODE::SGD,
+            "THB" => return Currency_CODE::THB,
+            "ZAR" => return Currency_CODE::ZAR,
             _ => Currency_CODE::NONE
         }
     }
@@ -90,12 +177,15 @@ impl Exchange {
         Ok(-1)
     }
 
-    pub fn get_ExchangeRate(self, target_Currency: Currency_CODE, date: NaiveDate) -> Currency_History_Entry {
-        let exchangeHistory:&Exchange_History = self.exchange_Histories.get(&target_Currency.to_str()).unwrap();
+    pub fn get_ExchangeRate(&self, target_Currency: Currency_CODE, date: NaiveDate) -> Currency_History_Entry {
+        let exchangeHistory: &Exchange_History = self.exchange_Histories.get(&target_Currency.to_str()).unwrap();
+
+        
+
         return self.search_exchangeRate(&exchangeHistory.exchange_entrys, date);
     }
 
-    fn search_exchangeRate(self, a: &Vec<Currency_History_Entry>, search_target: NaiveDate) -> Currency_History_Entry {
+    fn search_exchangeRate(&self, a: &Vec<Currency_History_Entry>, search_target: NaiveDate) -> Currency_History_Entry {
         let mut low: i64 = 0;
         let mut high: i64 = a.len() as i64;
         let mut mid = ((high - low) / 2) + low;
@@ -136,6 +226,36 @@ impl Exchange {
         match self.base_Currency{
             Currency_CODE::EUR => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.EUR..SP00.A".to_string(),
             Currency_CODE::USD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD..SP00.A".to_string(),
+            Currency_CODE::JPY => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.JPY..SP00.A".to_string(),
+            Currency_CODE::BGN => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.BGN..SP00.A".to_string(),
+            Currency_CODE::CZK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.CZK..SP00.A".to_string(),
+            Currency_CODE::DKK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.DKK..SP00.A".to_string(),
+            Currency_CODE::GBP => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.GBP..SP00.A".to_string(),
+            Currency_CODE::HUF => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.HUF..SP00.A".to_string(),
+            Currency_CODE::PLN => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.PLN..SP00.A".to_string(),
+            Currency_CODE::RON => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.RON..SP00.A".to_string(),
+            Currency_CODE::SEK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.SEK..SP00.A".to_string(),
+            Currency_CODE::CHF => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.CHF..SP00.A".to_string(),
+            Currency_CODE::ISK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.ISK..SP00.A".to_string(),
+            Currency_CODE::NOK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.NOK..SP00.A".to_string(),
+            Currency_CODE::HRK => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.HRK..SP00.A".to_string(),
+            Currency_CODE::RUB => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.RUB..SP00.A".to_string(),
+            Currency_CODE::TRY => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.TRY..SP00.A".to_string(),
+            Currency_CODE::AUD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.AUD..SP00.A".to_string(),
+            Currency_CODE::BRL => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.BRL..SP00.A".to_string(),
+            Currency_CODE::CAD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.CAD..SP00.A".to_string(),
+            Currency_CODE::CNY => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.CNY..SP00.A".to_string(),
+            Currency_CODE::HKD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.HKD..SP00.A".to_string(),
+            Currency_CODE::IDR => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.IDR..SP00.A".to_string(),
+            Currency_CODE::ILS => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.ILS..SP00.A".to_string(),
+            Currency_CODE::KRW => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.KRW..SP00.A".to_string(),
+            Currency_CODE::MXN => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.MXN..SP00.A".to_string(),
+            Currency_CODE::MYR => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.MYR..SP00.A".to_string(),
+            Currency_CODE::NZD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.NZD..SP00.A".to_string(),
+            Currency_CODE::PHP => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.PHP..SP00.A".to_string(),
+            Currency_CODE::SGD => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.SGD..SP00.A".to_string(),
+            Currency_CODE::THB => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.THB..SP00.A".to_string(),
+            Currency_CODE::ZAR => return "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.ZAR..SP00.A".to_string(),
             _ => "NONE".to_string()
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ fn main() {
 
     //test();
     //let return_value = hello_world(); // Nothing is printed
-    let mut Exchanger = Currency_Exchange::Exchange::new_enum(Currency_Exchange::Currency_CODE::USD);
-    let date = NaiveDate::from_ymd(2015, 3, 14);
+    let mut Exchanger = Currency_Exchange::Exchange::new_enum(Currency_Exchange::Currency_CODE::AUD);
+    let date = NaiveDate::from_ymd(2020, 1, 15);
     Exchanger.init();
     let rate = Exchanger.get_ExchangeRate(Currency_Exchange::Currency_CODE::EUR, date);
 
-    println!("wait");
+    println!("{:.64}", rate.OBS_VALUE.unwrap());
 }
